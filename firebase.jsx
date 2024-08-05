707cff567c8e877965e27ec78fb566135faf88a9
Mac <mac@Brians-Macbook.local>
2024-08-05 11:16:12 +0700

    initial commit

--- firebase.jsx
+++ firebase.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: "AIzaSyC9BLrhv5bdMoSmaEFxoFsDQr2QxhhrFDc",
-  authDomain: "inventory-tracker-f2b5d.firebaseapp.com",
-  projectId: "inventory-tracker-f2b5d",
-  storageBucket: "inventory-tracker-f2b5d.appspot.com",
-  messagingSenderId: "184964022291",
-  appId: "1:184964022291:web:555bab065cf4a2ef50bfe6",
-  measurementId: "G-7M7VST3WBG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
